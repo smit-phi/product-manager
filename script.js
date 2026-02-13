8f65686d97847d7a4ca2f7e22a38a1844b887ffe
smit-phi <smit.thakkar@simformsolutions.com>
2026-02-13 19:37:55 +0530

    feat: few functions in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const productList = []
+const productList = {}
 var id = 1;
 
 function Product(pid, name, image, price, description){
@@ -18,18 +18,79 @@ function Product(pid, name, image, price, description){
 
 }
 
+function toString(product){
+    return JSON.stringify(product);
+}
+
+function toObject(str){
+    return JSON.parse(str);
+}
+
 function addToLocalStorage(product){
-    let productString = JSON.stringify(product);
+    let productString = toString(product);
     window.localStorage.setItem(`${product.getPid()}`, productString);
 }
 
 function createProduct(name, image, price, description){
     let p = new Product(id, name, image, price, description);
-    id = id + 1;
+    productList[id] = toString(p);    
     addToLocalStorage(p);
+    id = id + 1;
     console.log(p);
 }
 
 function updateProduct(pid, name, image, price, description){
-    
+    window.localStorage.removeItem(`${pid}`);
+    let p = new Product(pid, name, image, price, description);
+    addToLocalStorage(p);
+    productList[pid] = toString(p);
+    console.log(p);
 }
+
+function filterByPid(startPid, endPid){
+
+    // if(endPid < startPid){
+        // return;
+    // }
+    // let start = 0;
+    // let end = productList.length - 1;
+    // if(endPid > end) return;
+
+    let filterdlist = {}
+    let curr = startPid;
+
+    while(curr != endPid + 1){
+        let obj = toObject(productList[curr])
+        filterdlist[curr] = obj
+        curr = curr + 1;
+    }
+    return filterdlist;    
+}
+
+createProduct('daftpunk helmet', 'url', 999, 'suit your self');
+createProduct('cricket helmet', 'url', 999, 'suit your own self');
+createProduct('mototcycle helmet', 'url', 999, 'save your self');
+
+
+let a = filterByPid(1, 2);
+console.log(typeof(a[1]));
+console.log(a);
+
+
+function sortByPid(products){
+    let sortedList = {}
+
+    
+
+
+
+}
+
+function sortByName(){
+
+}
+
+function sortByPrice(){
+
+}
+
